e488a7282155e252a85d700ccf77d495ab204e7b
sl1x101 <rew2540@gmail.com>
2026-02-25 08:57:25 +0900

    learn_array in python

---
+++ array.py
@@ -0,0 +1,10 @@
+#Array & list basic
+
+def  find_max(number):
+    max_val = number[0]
+    for num in number:
+        if num > max_val:
+            max_val = num
+    return max_val
+my_list = [10,5,20,15]
+print(f"max value:{find_max(my_list)}")
